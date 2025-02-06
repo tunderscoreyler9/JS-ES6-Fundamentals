e7dfe9cb2fb91ad8e5d46f1389ca909a3a13eb37
Tyler <tyler.s9@proton.me>
2025-02-06 18:16:37 -0500

    Array destructuring :)

---
+++ arrDestructuring.js
@@ -0,0 +1,17 @@
+const days = ['Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+];
+
+// const first = days[0];
+// const second = days[1];
+// const third = days[2];
+
+// Instead, use Array Destructuring:
+const [first, second, third] = days;
+
+console.log(first, second, third);
