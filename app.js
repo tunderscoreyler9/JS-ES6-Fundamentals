518a19de6c4fc0f96d0e89acc093b1ccd56d6185
Tyler <tyler.s9@proton.me>
2025-02-06 17:33:07 -0500

    Arrow functions again

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ console.log(greet("Mr. Adams"));
 // Single-line arrow function:
 const generateName = (name) => name;
 console.log(generateName("Andrew!"));
+
+const age = (value) => value + 10;
+
+console.log(age(30));
