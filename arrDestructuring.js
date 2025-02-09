d7d353c160e4090c5681631564da95f04be20612
Tyler <tyler.s9@proton.me>
2025-02-09 17:35:36 -0500

    Finished arr destructuring lesson

--- arrDestructuring.js
+++ arrDestructuring.js
@@ -7,11 +7,18 @@ const days = ['Monday',
     'Sunday'
 ];
 
+// Prior to ES6
 // const first = days[0];
 // const second = days[1];
 // const third = days[2];
 
 // Instead, use Array Destructuring:
 const [first, second, third] = days;
+
+// Skipping values
+const [, tuesday] = days;
+
+// Using the rest operator:
+const otherDays = [,,, ...others] = days; // Returns '['Thursday', 'Friday', 'Saturday', 'Sunday']'
 
 console.log(first, second, third);
